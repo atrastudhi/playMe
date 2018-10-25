40771daa868efededff34ab458a9af5ba7f698f7
Atras Tudhi <atras.r@gmail.com>
2018-10-26 00:30:22 +0700

    update

--- play.js
+++ play.js
@@ -1,3 +1,21 @@
 function show() {
-       document.getElementById('wizardGif').style.display='block';
+    var e = document.getElementById("selectheroes"); 
+    var strUser = e.options[e.selectedIndex].value; 
+    // console.log(strUser);
+    document.getElementById('header').style.display='none';
+
+    if(strUser === 'Knight') {
+        document.getElementById('knightGif').style.display='block';
+        document.getElementById('wizardGif').style.display='none';
+        document.getElementById('archerGif').style.display='none';
+    } else if(strUser === 'Wizard') {
+        document.getElementById('knightGif').style.display='none';
+        document.getElementById('wizardGif').style.display='block';
+        document.getElementById('archerGif').style.display='none';
+    } else if(strUser === 'Archer') {
+        document.getElementById('knightGif').style.display='none';
+        document.getElementById('wizardGif').style.display='none';
+        document.getElementById('archerGif').style.display='block';
+    }
+
 }
