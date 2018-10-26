3b49a118085c22c3e60ba49830cacdc3c513b14a
Atras Tudhi <atras.r@gmail.com>
2018-10-26 13:24:07 +0700

    final

--- play.js
+++ play.js
@@ -3,6 +3,7 @@ function show() {
     var strUser = e.options[e.selectedIndex].value; 
     // console.log(strUser);
     document.getElementById('header').style.display='none';
+    document.getElementById('table').style.display='block'
 
     if(strUser === 'Knight') {
         document.getElementById('knightGif').style.display='block';
@@ -11,7 +12,7 @@ function show() {
     } else if(strUser === 'Wizard') {
         document.getElementById('knightGif').style.display='none';
         document.getElementById('wizardGif').style.display='block';
-        document.getElementById('archerGif').style.display='none';
+        document.getElementById('archerGif').style.display='none';                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
     } else if(strUser === 'Archer') {
         document.getElementById('knightGif').style.display='none';
         document.getElementById('wizardGif').style.display='none';
@@ -19,3 +20,39 @@ function show() {
     }
 
 }
+
+function statPlus(stat) {
+    var totalPoint = Number(document.getElementById('totalPoint').innerHTML) - 1
+
+    if(stat === 'str' && totalPoint >= 0) {
+        var element = document.getElementById('plusStr')
+        var point = Number(element.innerHTML) + 1
+        element.innerHTML = point
+        document.getElementById('totalPoint').innerHTML = totalPoint
+    } else if(stat === 'agi' && totalPoint >= 0) {
+        var element = document.getElementById('plusAgi')
+        var point = Number(element.innerHTML) + 1
+        element.innerHTML = point
+        document.getElementById('totalPoint').innerHTML = totalPoint
+    } else if(stat === 'int' && totalPoint >= 0) {
+        var element = document.getElementById('plusInt')
+        var point = Number(element.innerHTML) + 1
+        element.innerHTML = point
+        document.getElementById('totalPoint').innerHTML = totalPoint
+    } else {
+        alert('Max Point!')
+    }
+    
+}
+
+// function statPlus() {
+//     var element = document.getElementById('plusStr')
+//     var point = Number(element.innerHTML) + 1
+//     element.innerHTML = point
+// }
+
+// function statPlus() {
+//     var element = document.getElementById('plusStr')
+//     var point = Number(element.innerHTML) + 1
+//     element.innerHTML = point
+// }
